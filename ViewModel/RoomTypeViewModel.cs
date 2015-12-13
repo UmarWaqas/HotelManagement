099680c8f62a526fc9a1996a7f87405faa1404ad
UmarWaqas <umarwaqas2010@gmail.com>
2015-12-14 00:13:10 +0500

    ViewModel Classes Fixed

---
+++ ViewModel/RoomTypeViewModel.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class RoomTypeViewModel : IDataErrorInfo
+    {
+         #region SetterAndGetters
+        private int id;
+        private string type;
+        private decimal charges;
+        private int capacity;
+
+        public int Capacity
+        {
+            set
+            {
+                capacity = value;
+            }
+            get
+            {
+                return capacity;
+            }
+        }
+
+        public decimal Charges
+        {
+            set
+            {
+                charges = value;
+            }
+            get
+            {
+                return charges;
+            }
+        }
+
+        public int Id
+        {
+            set
+            {
+                id = value;
+            }
+            get
+            {
+                return id;
+            }
+        }
+        public string Type
+        {
+            set
+            {
+                type = value;
+            }
+            get
+            {
+                return type;
+            }
+        }
+
+
+        #endregion
+
+        public string Error
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string this[string propertyName]
+        {
+            get
+            {
+                string result = null;
+
+                if (propertyName == "Type")
+                {
+                    if (string.IsNullOrEmpty(this.type))
+                    {
+                        result = "Room Type is required";
+                    }
+                }
+
+                else if (propertyName == "Capacity")
+                {
+                    if (this.capacity <= 0)
+                    {
+                        result = "Room Capacity is required";
+                    }
+                }
+
+                else if (propertyName == "Charges")
+                {
+                    if (this.charges <= 0)
+                    {
+                        result = "Number of Guests are required";
+                    }
+                }
+
+                return result;
+                throw new NotImplementedException();
+            }
+        }
+    }//end of class RoomTypeViewModel....
+    
+}//end of namespace....
---
+++ ViewModel/RoomViewModel.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class RoomViewModel : IDataErrorInfo
+    {
+        private int id{set; get;}
+        private int roomNumber;
+
+        public int RoomNumber
+        {
+            set{roomNumber=value;}
+            get{return roomNumber;}
+        }
+        private int managerId;
+        private int maintainerId;
+        public int MaintainerId
+        {
+            set { maintainerId = value; }
+            get { return maintainerId; }
+        }
+        public int ManagerId
+        {
+            set { managerId = value; }
+            get { return managerId; }
+        }
+        private int roomType;
+         public int RoomType
+        {
+            set{roomType=value;}
+            get{return roomType;}
+        }
+
+
+
+
+         public string Error
+         {
+             get { throw new NotImplementedException(); }
+         }
+
+         public string this[string propName]
+         {
+             
+                  get 
+            {
+                string result = null;
+
+                if (propName == "RoomType")
+                {
+                    if (this.roomType<=0)
+                    {
+                        result = "Room Type is required";
+                    }
+                }
+                else if (propName == "RoomNumber")
+                {
+                    if (this.roomNumber <= 199)
+                    {
+                        result = "Room Number  is required";
+                    }
+                }
+
+                else if (propName == "ManagerId")
+                {
+                    if (this.managerId <= 0)
+                    {
+                        result = "Manager Name is required";
+                    }
+                }
+
+                else if (propName == "MaintainerId")
+                {
+                    if (this.maintainerId <= 0)
+                    {
+                        result = "Maintainer Name is required";
+                    }
+                }
+
+                return result;
+                      throw new NotImplementedException(); }
+         }
+    }
+}
